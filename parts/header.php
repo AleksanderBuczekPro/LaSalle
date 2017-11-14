22b505d4e8a0c16b9fce1f374b6a00856a27b1b4
Unknown <timotesibellas@gmail.com>
2017-11-14 14:13:51 +0100

    patch connexion

--- parts/header.php
+++ parts/header.php
@@ -38,40 +38,6 @@
                         </li>
                         <li class="nav-item">
                             <a class="nav-link page-scroll text-primary" data-toggle="modal" data-target="#myModal">Connexion</a>
-
-                            <!-- Modal -->
-                            <div id="myModal" class="modal fade" role="dialog">
-                                <div class="modal-dialog">
-                                    <!-- Modal content-->
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                        </div>
-                                        <div class="modal-body">
-                                            <form  class="container form-horizontal">
-                                                <h2 class="text-center">Connexion</h2>
-                                                <div class="row form-group">
-                                                    <label class="control-label col-md-3" for="email">Email :</label>
-                                                    <div class="col-md-9">
-                                                        <input type="text" id="email" class="form-control" placeholder="Adresse mail" required/>
-                                                    </div>
-                                                </div>
-                                                <div class="row form-group">
-                                                    <label class="control-label col-md-3" for="mdp">Mot de passe :</label>
-                                                    <div class="col-md-9">  
-                                                        <input type="password" id="mdp" class="form-control" placeholder="Password" required/>
-                                                    </div>
-                                                </div>
-                                                <div class="text-center">
-                                                <button type="submit" class="btn btn-outline-secondary">Connexion</button>
-                                                </div>
-                                            </form>
-                                        </div>
-                                        <div class="modal-footer">
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
                         
                         </li>
                     </ul>
