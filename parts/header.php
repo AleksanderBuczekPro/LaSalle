1181890fa5ee48e992f5d862267cf18741077cd9
Unknown <timotesibellas@gmail.com>
2017-11-16 09:42:16 +0100

    Connexion fonctionnelle

--- parts/header.php
+++ parts/header.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+<?php session_start(); ?>
 <html lang="en">
     <head>
         <meta charset="utf-8">
@@ -38,40 +39,45 @@
                             <a class="nav-link page-scroll" href="index.php#contact">Contact</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link page-scroll text-primary" data-toggle="modal" data-target="#myModal" href="#myModal">Connexion</a>
-                            <!-- Modal -->
-                            <div id="myModal" class="modal fade" role="dialog">
-                                <div class="modal-dialog">
-                                    <!-- Modal content-->
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                        </div>
-                                        <div class="modal-body">
-                                            <form  class="container form-horizontal" method="POST" action="parts/connexion.php">
-                                                <h2 class="text-center">Connexion</h2>
-                                                <div class="row form-group">
-                                                    <label class="control-label col-md-3" for="email">Email :</label>
-                                                    <div class="col-md-9">
-                                                        <input type="text" name="email" class="form-control" placeholder="Adresse mail" required/>
+                            <?php if (isset($_SESSION['login'])): ?>
+                                <a class="nav-link page-scroll" href="parts/deconnexion.php">Déconnexion</a>
+                                <p>Bonjour <?php echo $_SESSION['login']; ?> .</p>
+                            <?php else: ?>
+                                <a class="nav-link page-scroll text-primary" data-toggle="modal" data-target="#myModal" href="#myModal">Connexion</a>
+                                <!-- Modal -->
+                                <div id="myModal" class="modal fade" role="dialog">
+                                    <div class="modal-dialog">
+                                        <!-- Modal content-->
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <form  class="container form-horizontal" method="POST" action="parts/connexion.php">
+                                                    <h2 class="text-center">Connexion</h2>
+                                                    <div class="row form-group">
+                                                        <label class="control-label col-md-3" for="email">Email :</label>
+                                                        <div class="col-md-9">
+                                                            <input type="text" name="email" class="form-control" placeholder="Adresse mail" required/>
+                                                        </div>
                                                     </div>
-                                                </div>
-                                                <div class="row form-group">
-                                                    <label class="control-label col-md-3" for="motdepasse">Mot de passe :</label>
-                                                    <div class="col-md-9">  
-                                                        <input type="password" name="motdepasse" class="form-control" placeholder="Password" required/>
+                                                    <div class="row form-group">
+                                                        <label class="control-label col-md-3" for="motdepasse">Mot de passe :</label>
+                                                        <div class="col-md-9">  
+                                                            <input type="password" name="motdepasse" class="form-control" placeholder="Password" required/>
+                                                        </div>
                                                     </div>
-                                                </div>
-                                                <div class="text-center">
-                                                <button type="submit" class="btn btn-outline-secondary">Connexion</button>
-                                                </div>
-                                            </form>
-                                        </div>
-                                        <div class="modal-footer">
+                                                    <div class="text-center">
+                                                    <button type="submit" class="btn btn-outline-secondary">Connexion</button>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                            <div class="modal-footer">
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
+                            <?php endif; ?>
                         </li>                   
                     </ul>
                 </div>
