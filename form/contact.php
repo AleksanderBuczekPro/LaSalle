af78c3d6adf4241d0fd2a02faffb6c26f8f4cb04
Unknown <buczek.aleksander@gmail.com>
2017-12-14 10:24:01 +0100

    Revert "Modification Front Index"
    This reverts commit a342b3434e02d9215630c05768f4ae9047181925.

--- form/contact.php
+++ form/contact.php
@@ -1,45 +1,24 @@
 <?php
-    	
-	// Ma clé privée
-	$secret = "6LeA5jwUAAAAAGFRrn6H9sbTtUtQ9v-8hhCm7-5G";
-	// Paramètre renvoyé par le recaptcha
-	$response = $_POST['g-recaptcha-response'];
-	// On récupère l'IP de l'utilisateur
-	$remoteip = $_SERVER['REMOTE_ADDR'];
-	
-	$api_url = "https://www.google.com/recaptcha/api/siteverify?secret=" 
-	    . $secret
-	    . "&response=" . $response
-	    . "&remoteip=" . $remoteip ;
-	
-	$decode = json_decode(file_get_contents($api_url), true);
-	
-	if ($decode['success'] == true) {
-		// C'est un humain
-		
-		$nom = $_POST['nom'];
-		$email = $_POST['email'];
-		$message = $_POST['message'];
 
-		$connecteur = new PDO('mysql:host=localhost;dbname=lasalle', 'root', ''); 
+	$nom = $_POST['nom'];
+	$email = $_POST['email'];
+	$message = $_POST['message'];
 
-		$connecteur->exec("INSERT INTO formulaire (nom, email, message, dateEnvoi) VALUES ('". $nom . "', '" . $email . "', '" . $message . "', NOW())");
+	$connecteur = new PDO('mysql:host=localhost;dbname=salle_fitness', 'salledesport', 'hopeforce3'); 
 
-		/*print_r($connecteur->errorInfo());
+	$connecteur->exec("INSERT INTO formulaire (nom, email, message, dateEnvoi) VALUES ('". $nom . "', '" . $email . "', '" . $message . "', NOW())");
 
-		$date = date('d-m-Y');
+	/*print_r($connecteur->errorInfo());
+
+	$date = date('d-m-Y');
 
 
-		print_r($date);
+	print_r($date);
 
-		var_dump($nom, $email, $message);*/
-
-		mail("hector.barre@protonmail.com","Test mail", $message);
-		print phpinfo();  
-		//mail("hector.barre@protonmail.com", "Form to email message", $_POST["message"], "From: an@email.address");
-	}
+	var_dump($nom, $email, $message);*/
 
 
-	header("Location: ../index.php");	
+
+	header("Location: ../index.php");
 
 ?>
